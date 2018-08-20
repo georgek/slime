81061c63edb2dffd5a2402f54f027848ca788f91
George Kettleborough <kettleg@gmail.com>
2018-08-20 14:09:29 +0100

    Delete autoloads

--- slime-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; slime-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "slime" "slime.el" (0 0 0 0))
-;;; Generated autoloads from slime.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "slime" '("sldb-" "slime" "def-slime-selector-method" "define-sl" "??" "?q" "?i" "?v" "?l" "?d" "?e" "?c" "?n" "?p" "?t" "make-slime-" "inferior-lisp-program")))
-
-;;;***
-
-;;;### (autoloads nil nil ("slime-tests.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; slime-autoloads.el ends here
